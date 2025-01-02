796755d3d310f54b96da89739f0fddc9a8b473a5
Davide Vercelli <vercellidavide01@gmail.com>
2025-01-02 10:08:16 +0100

    added the route to find all the reviews

--- routes/review.js
+++ routes/review.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
 
 });
 
+
+router.get('/reviews/all', async function(req, res, next) {
+    try {
+        const db = req.app.locals.db;
+
+        const allReviews = await db.collection('Reviews').aggregate([
+            { $unwind: '$reviews' },
+            { $replaceRoot: { newRoot: '$reviews' } }
+        ]).toArray();
+
+        res.json(allReviews);
+
+    } catch (error) {
+        console.error('Error getting all reviews', error);
+        res.status(500).json({ error: 'Error getting all reviews.' });
+    }
+})
+
+
 module.exports = router;
